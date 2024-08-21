eb96b64508e58d8c798c3d93036fc3470daf286a
Marlon Montenegro <marlonmontenegropaz@gmail.com>
2024-08-21 14:28:48 -0600

    First Class, variables

---
+++ Variables/variables.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {
+
+    // Integer (Whole Number)
+    int x; // declaration
+    x = 5; /// assignment
+
+    int y = 5;
+    int sum = x + y;
+
+    std::cout << x; 
+
+    //Double (Number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+     std::cout << price; 
+
+     // single character
+     char grade = 'A';
+     char initial = 'B';
+     char currency = '$';
+
+     std::cout << initial;
+
+
+    // boolean (True or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    // String (Objects that represaent a sequence of text)
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food = "pizza";
+
+    std::cout << "Hello " << name;
+
+
+
+}
